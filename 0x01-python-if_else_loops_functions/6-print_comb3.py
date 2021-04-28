b5270d5b9465988815818a94c17a7e0e9a3e5267
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-27 19:40:18 -0500

    xd

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for x in range(10):
     for j in range(10):
-        if x != j and (x < j) and ((x * 10) + j) < 89:
+        if (x != j) and (x < j) and ((x * 10) + j) < 89:
             print("{:d}{:d}".format(x, j), end=", ")
         elif ((x * 10) + j) == 89:
-            print("{:d}{:d}".format(x, j), end="")
-print()
+            print("{:d}{:d}".format(x, j))
