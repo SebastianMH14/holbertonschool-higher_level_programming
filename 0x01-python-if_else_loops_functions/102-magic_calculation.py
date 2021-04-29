66fc3425ce89404780c58fbe0ab63a757a8cf63a
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-29 13:19:08 -0500

    fix

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a<b:
-        return c
-    elif c>b:
-        return a+b
-    return a * b - c
-
 import dis
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    return a * b - c
 dis.dis(magic_calculation)
