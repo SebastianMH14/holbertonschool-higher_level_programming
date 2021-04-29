614c509f67ad35c175cf2d6d77665756e6e46b7d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-29 13:54:35 -0500

    tkas 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == '__main__':
+    import add_0 as suma
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a + b))
-
-__import__("0-add")
+    print("{:d} + {:d} = {}".format(a, b, suma.add(a, b)))
