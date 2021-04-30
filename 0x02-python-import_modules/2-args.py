a8e84328640ea32a4ab400011410184344254cf6
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-30 12:15:15 -0500

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ n = len(argv)
 
 if n == 1:
     print("0 arguments.")
-    if n == 2:
-        print("{:d} argument: ".format(n - 1))
+elif n == 2:
+    print("{:d} argument: ".format(n - 1))
 else:
     print("{:d} arguments: ".format(n - 1))
 for i in range(1, n):
