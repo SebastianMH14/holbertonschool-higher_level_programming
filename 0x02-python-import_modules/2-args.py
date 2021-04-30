78fad001edf589286cdaf3a0f819b160f0617c17
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-30 12:10:16 -0500

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+n = len(argv)
+
+if n == 1:
+    print("0 arguments.")
+    if n == 2:
+        print("{:d} argument: ".format(n - 1))
+else:
+    print("{:d} arguments: ".format(n - 1))
+    for i in range(1, n):
+        print("{:d}:".format(i), argv[i])
