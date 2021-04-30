df66a1e50a2570f1b75cffe45dff22630e77c734
SebastianMH14 <sebasmh2002@hotmail.com>
2021-04-30 12:18:50 -0500

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     if n == 1:
         print("0 arguments.")
     elif n == 2:
-        print("{:d} argument: ".format(n - 1))
+        print("{:d} argument:".format(n - 1))
     else:
-        print("{:d} arguments: ".format(n - 1))
+        print("{:d} arguments:".format(n - 1))
     for i in range(1, n):
         print("{:d}:".format(i), argv[i])
