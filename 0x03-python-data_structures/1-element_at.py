a9755d8eaa0378162e928b15f31036ebf0157fca
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-06 10:52:05 -0500

    hellop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,5 @@
 def element_at(my_list, idx):
     if idx == None:
         return None
-    for i in my_list:
-        if idx > my_list[i]:
-            None
-            return my_list[idx]
+    for i in range(idx):
+        return my_list[i]
