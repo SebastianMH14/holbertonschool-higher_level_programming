cdbcbfd810f506d5d6affd79a2aae2ac229849a9
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-06 10:45:52 -0500

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx == None:
+        return None
+    for i in my_list:
+        if idx > my_list[i]:
+            None
+            return my_list[idx]
