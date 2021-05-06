7cee0fbe18fdb9443492cfbcdaccc0bce8e598f5
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-06 14:07:09 -0500

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     a = len(sentence)
     if len(sentence) == 0:
-        a = None
-    b = sentence[0]
-    if a != 0:
-        return (a, b)
+        b = None
+    else:
+        b = sentence[0]
+    return (a, b)
