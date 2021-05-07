6321419388f5548842c4edfb3a328c2f291bfe36
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-06 19:39:11 -0500

    no c task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    counts = my_string.count("c")
-    my_string = list(my_string)
-    while counts:
-        my_string.remove("c")
-        counts -= 1
-    my_string = ''.join(my_string)
+    counts_c = my_string.count("c")
+    counts_C = my_string.count("C")
+    if counts_c > 0:
+        my_string = list(my_string)
+
+        while counts_c:
+            my_string.remove("c")
+            counts_c -= 1
+        my_string = ''.join(my_string)
+    if counts_C > 0:
+        my_string = list(my_string)
+
+        while counts_C:
+            my_string.remove("C")
+            counts_C -= 1
+        my_string = ''.join(my_string)
+    return my_string
