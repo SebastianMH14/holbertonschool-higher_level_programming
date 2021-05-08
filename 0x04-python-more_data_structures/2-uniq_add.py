a9d649fe720bbe4d716579ca8aa6dff0f3b96c41
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-08 15:25:10 -0500

    uniq taks 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    suma = 0
-    for i in range(0, len(my_list)):
-        if my_list[i] != my_list[2]:
-            suma = suma + my_list[i]
+    suma = sum(set(my_list))
     return suma
