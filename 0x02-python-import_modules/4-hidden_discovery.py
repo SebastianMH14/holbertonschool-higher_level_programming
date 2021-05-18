d7736ef9de8e4980abf42206695c1161bae936ca
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-18 09:58:09 -0500

    fix task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    hid = dir(hidden_4)
-    for x in range(len(hid)):
-        if hid[x][0] != "_":
-            print(hid[x])
+    for c in dir(hidden_4):
+        if c[0] != "_":
+            print("{}".format(c))
