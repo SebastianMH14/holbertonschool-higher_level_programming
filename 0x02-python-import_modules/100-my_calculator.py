449eceed07c1f8a249e027e149a5e8835026e62d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-18 10:05:18 -0500

    fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     import sys
-    av = sys.argv
+    ar = sys.argv
     lenAv = len(sys.argv)
     if lenAv != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if av[2] != '+' and av[2] != '-' and av[2] != '*' and av[2] != '/':
+    if ar[2] != '+' and ar[2] != '-' and ar[2] != '*' and ar[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    a = int(av[1])
-    b = int(av[3])
-    if av[2] == '+':
-        print(a, av[2], b, "=", add(a, b))
-    if av[2] == '-':
-        print(a, av[2], b, "=", sub(a, b))
-    if av[2] == '*':
-        print(a, av[2], b, "=", mul(a, b))
-    if av[2] == '/':
-        print(a, av[2], b, "=", div(a, b))
+    a = int(ar[1])
+    b = int(ar[3])
+    if ar[2] == '+':
+        print(a, ar[2], b, "=", add(a, b))
+    if ar[2] == '-':
+        print(a, ar[2], b, "=", sub(a, b))
+    if ar[2] == '*':
+        print(a, ar[2], b, "=", mul(a, b))
+    if ar[2] == '/':
+        print(a, ar[2], b, "=", div(a, b))
