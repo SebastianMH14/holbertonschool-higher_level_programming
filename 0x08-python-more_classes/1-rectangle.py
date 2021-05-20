659605a94ac5306c0f6823655407897862be213f
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-20 16:24:25 -0500

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """rectanlge class"""
+    def __init__(self, width=0, height=0):
+        """init class"""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """ accediendo a atributo privado"""
+        return self.__height
+
+    @property
+    def width(self):
+        """accediendo a atributo privado"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if isinstance(value, int) is True:
+            self._Rectangle__width = value
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if isinstance(value, int) is True:
+            self._Rectangle__height = value
+        else:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
