38dce15911bd39a22a3dd904ee3eb59050c15b57
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-20 21:32:53 -0500

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """rectanlge class"""
     def __init__(self, width=0, height=0):
         """constructor de clase"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def height(self):
