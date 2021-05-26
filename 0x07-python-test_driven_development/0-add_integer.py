4e1d4825c86cd8c3797ec543636ee9df96d43bb4
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-26 10:15:51 -0500

    pep8 fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """function that adds 2 integers.
-    a and b must be integers or floats, 
-    otherwise raise a TypeError exception 
-    with the message a must be an integer or b 
+    a and b must be integers or floats,
+    otherwise raise a TypeError exception
+    with the message a must be an integer or b
     must be an integer"""
 
 
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
     if not raise error"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)): 
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
