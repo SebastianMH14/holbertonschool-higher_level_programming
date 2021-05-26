90ce7375272c9f1a75967d057e5cd2a20e575fa2
SebastianMH14 <sebasmh2002@hotmail.com>
2021-05-26 13:41:37 -0500

    fix errors

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,11 +7,8 @@ def say_my_name(first_name, last_name=""):
     """first_name and last_name must be strings
     otherwise, raise a TypeError exception with the message
     irst_name must be a string or last_name must be a string"""
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    else:
-        print("My name is {:s}".format(first_name), end=" ")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    else:
-        print("{:s}".format(last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
