704c297d5698376bc125a56d46df0fee0cf32ff9
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-01 15:46:17 -0500

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """creating a class"""
+
+    def __init__(self):
+        """creating constructor"""
+        pass
+
+    def area(self):
+        """raises an Exception with the
+        message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """that validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
