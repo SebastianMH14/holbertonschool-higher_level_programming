2a684cdaf5e837b37a139d8de50b2ba1b1e3bc86
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-01 13:55:13 -0500

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""function that returns the list
+ of available attributes and methods
+ of an object"""
+
+
 def lookup(obj):
-    a = list (dir(obj))
+    """create a funtion with dir"""
+    a = list(dir(obj))
     return a
