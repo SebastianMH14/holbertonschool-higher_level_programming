07ebb09ba61872271d6e3135ed3f795ea8a1cf46
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-01 15:31:07 -0500

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """creating a class"""
+
+    def __init__(self):
+        """creating constructor"""
+        pass
+
+    def area(self):
+        """raises an Exception with the
+        message area() is not implemented"""
+        raise Exception("area() is not implemented")
