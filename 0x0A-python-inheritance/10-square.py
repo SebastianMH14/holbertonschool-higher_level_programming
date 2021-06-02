67f7a3c516490ed99aa269dad8cdb8c65b4727df
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-01 22:06:15 -0500

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a class Square that inherits
+from Rectangle (9-rectangle.py)"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """creating a class"""
+
+    def __init__(self, size):
+        """constructor de clase"""
+        self.__size = size
+        self.integer_validator("size", self.__size)
+
+    def area(self):
+        """obtener el area"""
+        return (self.__size * self.__size)
