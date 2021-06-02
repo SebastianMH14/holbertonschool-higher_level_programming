2eaf3b92e29bd6c5650d37504c56d0c6ba339263
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-01 20:56:06 -0500

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     """class MyList that inherits from list"""
+    def __init__(self):
+        """constructor de clase"""
+        super().__init__
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
