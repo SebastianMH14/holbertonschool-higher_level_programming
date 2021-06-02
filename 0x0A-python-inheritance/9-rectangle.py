0c7532dfccc486c8da9f1d82a39f87ddf684d3f2
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-01 21:55:54 -0500

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a class Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """creating a class"""
+
+    def __init__(self, width, height):
+        """constructor de clase"""
+        self.__width = width
+        self.__heigth = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("heigth", self.__heigth)
+
+    def area(self):
+        """obtener area"""
+        return self.__width * self.__heigth
+
+    def __str__(self):
+        """Rectangle description: [Rectangle] <width>/<height>"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__heigth)
