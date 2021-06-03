0641671faf481fc38c05f81355fa2bdd21ea6715
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-02 22:05:12 -0500

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""writes a string to a text file
+(UTF8) and returns the
+number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """use the with statement"""
+    with open(filename, 'a', encoding="utf-8") as file:
+        a = file.write(text)
+        return a
