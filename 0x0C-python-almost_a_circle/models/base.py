13f5cf8f6576e764e36821a720cbdf287c7300f8
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-08 23:48:52 -0500

    add some task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """Class Base private class
 attribute __nb_objects = 0
 class constructor"""
+import json
+from os import write
 
 
 class Base:
@@ -15,3 +17,41 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return list in json"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method to file
+        in json string"""
+        if list_objs is None:
+            list_objs = []
+        dic_t = []
+        for a in list_objs:
+            dic_t.append(cls.to_dictionary(a))
+        with open(cls.__name__ + ".json", "w+") as file:
+            file.write(Base.to_json_string(dic_t))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """add static method"""
+        if json_string is None:
+            return[]
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """dummy element, method"""
+        if cls.__name__ == "Square":
+            dummy = cls(5, 5)
+        else:
+            dummy = cls(5)
+        dummy.update(**dictionary)
+        return dummy
