67d4cca7b0d46ebf48840469e3859bc24a362a45
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-08 20:29:36 -0500

    add args and kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,3 +94,22 @@ class Rectangle(Base):
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """update metod with args"""
+        n = len(args)
+        if n > 0:
+            for i in range(n):
+                if i == 0:
+                    setattr(self, "id", args[0])
+                if i == 1:
+                    setattr(self, "width", args[1])
+                if i == 2:
+                    setattr(self, "height", args[2])
+                if i == 3:
+                    setattr(self, "x", args[3])
+                if i == 4:
+                    setattr(self, "y", args[4])
+            else:
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
