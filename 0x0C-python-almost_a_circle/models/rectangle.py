55da70a7e3923a83a9c62e7b1d748411c79d71bd
SebastianMH14 <sebasmh2002@hotmail.com>
2021-06-08 21:43:57 -0500

    fix dict

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,6 +118,6 @@ class Rectangle(Base):
         """adding metod"""
         dic_t = {}
         for k in self.__dict__:
-            k_n = k.replace("_Rctangle_", "")
+            k_n = k.replace("_Rctangle__", "")
             dic_t[k_n] = self.__dict__[k]
         return dic_t
