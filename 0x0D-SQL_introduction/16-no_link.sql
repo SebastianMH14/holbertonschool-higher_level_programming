af20d3dc9fcc988094a4890624fcd19202502b94
SebastianMH14 <sebasmh2002@hotmail.com>
2021-07-12 19:13:33 -0500

    no link

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table 
-SELECT score, name FROM second_table WHERE name EXISTS ORDER BY score DESC; 
+SELECT score, name FROM second_table WHERE name <> TRUE ORDER BY score DESC; 
