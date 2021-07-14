46729c7b0852747e5787377ab599fb9df5ca5163
SebastianMH14 <sebasmh2002@hotmail.com>
2021-07-13 19:19:12 -0500

    INNER JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- script that lists all cities contained in the database
+-- script that lists all cities contained
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.id = states.id
