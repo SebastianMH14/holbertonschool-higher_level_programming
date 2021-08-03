92c7314f67b0953b732ef690bdbe1ac18b8143e0
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-02 20:40:41 -0500

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const arg = process.argv;
+
+const x = parseInt(arg[2]);
+
+if (x) {
+  for (let i = 0; i < x; i++) {
+    let square = 'X';
+    for (let i = 1; i < x; i++) {
+      square = square + 'X';
+    }
+    console.log(square);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
