350298ad5fed9261726ef3e619624d2ea6196310
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-05 18:37:33 -0500

    fix advanced task

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,8 +4,10 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function incr () {
-  myObject.value += 1;
+
+myObject.incr = function () {
+  const i = myObject.value = myObject.value + 1;
+  return i;
 };
 
 myObject.incr();
