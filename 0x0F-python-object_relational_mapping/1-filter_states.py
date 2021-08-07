71c40d8eb3b82e36643310f2397ed2a63a9c502d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-07 10:02:42 -0500

    add if name ==main

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,24 +3,26 @@
 states from the database
 hbtn_0e_0_usa"""
 
-import MySQLdb
-from sys import argv
+if __name__ == '__main__':
 
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
+    import MySQLdb
+    from sys import argv
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=user,
-    passwd=passwd,
-    db=db,
-    charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name like 'N%'  ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db,
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'N%'  ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
