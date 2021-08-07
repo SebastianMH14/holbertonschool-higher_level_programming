53e11fafea762ec781b4be32860681ca10eafa8d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-07 10:05:43 -0500

    add if name == main

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,27 +3,28 @@
 states from the database
 hbtn_0e_0_usa"""
 
-import MySQLdb
-from sys import argv
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
 
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
 
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=user,
-    passwd=passwd,
-    db=db,
-    charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name  \
-    FROM cities INNER JOIN states \
-    ON states.id = cities.state_id \
-    ORDER BY cities.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db,
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name  \
+        FROM cities INNER JOIN states \
+        ON states.id = cities.state_id \
+        ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
