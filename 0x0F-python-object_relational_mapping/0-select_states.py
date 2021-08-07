abd77e19460e410223f7ed8e544f44dee43df09d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-07 10:00:28 -0500

    fix spaces

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,12 +13,12 @@ if __name__ == '__main__':
     db = argv[3]
 
     conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=user,
-    passwd=passwd,
-    db=db,
-    charset="utf8")
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=passwd,
+        db=db,
+        charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
