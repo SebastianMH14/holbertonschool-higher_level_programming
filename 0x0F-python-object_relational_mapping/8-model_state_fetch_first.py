dea2b4a8251052af543b83f2e65a13ea922aba2d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-07 16:55:34 -0500

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,9 +23,9 @@ if __name__ == '__main__':
     Session = sessionmaker(engine)
     session = Session()
 
-    first_query = session.query(State).first()
+    first_query = session.query(State ).first()
 
-    if first_query == null:
+    if first_query is null:
         print("Nothing")
     else:
         print("{}: {}".format(first_query.id, first_query.name))
