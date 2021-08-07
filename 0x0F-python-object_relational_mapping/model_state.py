6366f8331fe6988ed4032b4998b6bb09e069e939
SebastianMH14 <sebasmh2002@hotmail.com>
2021-08-07 12:03:49 -0500

    fix nullable

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,6 @@
 """python file that contains
 the class definition of a State
 and an instance Base = declarative_base()"""
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,5 +11,5 @@ Base = declarative_base()
 class State(Base):
     """table state"""
     __tablename__ = 'state'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
