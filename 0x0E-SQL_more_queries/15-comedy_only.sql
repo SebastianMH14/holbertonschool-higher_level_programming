a22d4966bcd6e74ed566b469474af43ba9ab5f86
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-08 10:28:58 -0500

    add task 15-16

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,10 @@
--- Import the database dump from hbtn_0d_tvshows
--- script that lists all Comedy shows
+-- List all comedies,
+--  INNER JOIN
 SELECT tv_shows.title
-FROM tv_shows_genres
-INNER JOIN tv_shows
-ON tv_shows_genres.show_id = tv_shows.id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
