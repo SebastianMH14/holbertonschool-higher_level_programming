23f9d794875bbcbf77606035ca106eb0a0e01b8d
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-14 13:17:16 -0500

    add task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const arg = process.argv;
+
+request
+  .get(arg[2])
+  .on('response', function (response) {
+    console.log('code:', response.statusCode); // 200
+  });
