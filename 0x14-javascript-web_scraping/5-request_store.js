0f48e894e47146e3a338bc56ece87f5cbe169311
SebastianMH14 <sebasmh2002@hotmail.com>
2021-09-14 22:15:46 -0500

    fix task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,14 +3,13 @@ const arg = process.argv;
 const request = require('request');
 const fs = require('fs');
 
-request.get({ url: arg[2], json: true },
-  function (error, a, body) {
-    if (error) {
-      console.error(error);
+request.get({ url: arg[2] }, function (error, r, body) {
+  if (error) {
+    console.error(error);
+  }
+  fs.writeFile(arg[3], body, err => {
+    if (err) {
+      console.error(err);
     }
-    fs.writeFile(arg[3], body, err => {
-      if (err) {
-        console.error(err);
-      }
-    });
   });
+});
